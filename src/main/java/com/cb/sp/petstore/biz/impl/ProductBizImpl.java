fbd1a33b54d07594af50059f565c88a348713eb8
YangTao <2677250566@qq.com>
2018-12-12 14:42:40 +0800

    petstore

--- src/main/java/com/cb/sp/petstore/biz/impl/ProductBizImpl.java
+++ src/main/java/com/cb/sp/petstore/biz/impl/ProductBizImpl.java
@@ -1,12 +1,14 @@
 package com.cb.sp.petstore.biz.impl;
 
 import com.cb.sp.petstore.biz.ProductBiz;
+import org.springframework.stereotype.Component;
 
 /**
  * @author 杨涛
  * @description TODO
  * @date 2018/12/11 17:50
  */
+@Component("productBiz")
 public class ProductBizImpl implements ProductBiz {
 
 }
--- src/main/java/com/cb/sp/petstore/facade/ProductFacade.java
+++ src/main/java/com/cb/sp/petstore/facade/ProductFacade.java
@@ -1,10 +1,16 @@
 package com.cb.sp.petstore.facade;
 
+import com.cb.sp.petstore.dto.ProductDto;
+
+import java.util.List;
+
 /**
  * @author 杨涛
  * @description TODO
  * @date 2018/12/11 17:50
  */
 public interface ProductFacade {
+
+    List<ProductDto> getProductList(ProductDto productDto);
 
 }
