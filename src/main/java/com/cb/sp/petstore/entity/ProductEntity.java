69207caec564c8ac0fb7a5b3c13c2895e6fd5c63
YangTao <2677250566@qq.com>
2018-12-26 21:15:27 +0800

    yangtao

--- src/main/java/com/cb/sp/petstore/entity/ProductEntity.java
+++ src/main/java/com/cb/sp/petstore/entity/ProductEntity.java
@@ -1,12 +1,16 @@
 package com.cb.sp.petstore.entity;
 
-import java.math.BigDecimal;
-
 public class ProductEntity{
 
 	private	Integer	productId;
 
 	private	String	productName;
+
+	private	String	pic1;
+
+	private	String	pic2;
+
+	private	String	pic3;
 
 	private	Long	price;
 
@@ -34,123 +38,147 @@ public class ProductEntity{
 
 	private	Byte	productIsDelete;
 
-	public Integer getProductId() {
-		return productId;
+	public	Integer	getProductId(){
+		return	productId;
 	}
 
-	public void setProductId(Integer productId) {
+	public	String	getProductName(){
+		return	productName;
+	}
+
+	public	String	getPic1(){
+		return	pic1;
+	}
+
+	public	String	getPic2(){
+		return	pic2;
+	}
+
+	public	String	getPic3(){
+		return	pic3;
+	}
+
+	public	Long	getPrice(){
+		return	price;
+	}
+
+	public	Integer	getNumber(){
+		return	number;
+	}
+
+	public	String	getIntroduction(){
+		return	introduction;
+	}
+
+	public	Byte	getType(){
+		return	type;
+	}
+
+	public	Integer	getAge(){
+		return	age;
+	}
+
+	public	String	getVariety(){
+		return	variety;
+	}
+
+	public	String	getArea(){
+		return	area;
+	}
+
+	public	Byte	getRecommendation(){
+		return	recommendation;
+	}
+
+	public	Integer	getSalesVolum(){
+		return	salesVolum;
+	}
+
+	public	String	getCharacters(){
+		return	characters;
+	}
+
+	public	String	getProductFunction(){
+		return	productFunction;
+	}
+
+	public	String	getProductShape(){
+		return	productShape;
+	}
+
+	public	Byte	getProductIsDelete(){
+		return	productIsDelete;
+	}
+
+	public void	setProductId(Integer productId){
 		this.productId = productId;
 	}
 
-	public String getProductName() {
-		return productName;
-	}
-
-	public void setProductName(String productName) {
+	public void	setProductName(String productName){
 		this.productName = productName;
 	}
 
-	public Long getPrice() {
-		return price;
+	public void	setPic1(String pic1){
+		this.pic1 = pic1;
 	}
 
-	public void setPrice(Long price) {
+	public void	setPic2(String pic2){
+		this.pic2 = pic2;
+	}
+
+	public void	setPic3(String pic3){
+		this.pic3 = pic3;
+	}
+
+	public void	setPrice(Long price){
 		this.price = price;
 	}
 
-	public Integer getNumber() {
-		return number;
-	}
-
-	public void setNumber(Integer number) {
+	public void	setNumber(Integer number){
 		this.number = number;
 	}
 
-	public String getIntroduction() {
-		return introduction;
-	}
-
-	public void setIntroduction(String introduction) {
+	public void	setIntroduction(String introduction){
 		this.introduction = introduction;
 	}
 
-	public Byte getType() {
-		return type;
-	}
-
-	public void setType(Byte type) {
+	public void	setType(Byte type){
 		this.type = type;
 	}
 
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
+	public void	setAge(Integer age){
 		this.age = age;
 	}
 
-	public String getVariety() {
-		return variety;
-	}
-
-	public void setVariety(String variety) {
+	public void	setVariety(String variety){
 		this.variety = variety;
 	}
 
-	public String getArea() {
-		return area;
-	}
-
-	public void setArea(String area) {
+	public void	setArea(String area){
 		this.area = area;
 	}
 
-	public Byte getRecommendation() {
-		return recommendation;
-	}
-
-	public void setRecommendation(Byte recommendation) {
+	public void	setRecommendation(Byte recommendation){
 		this.recommendation = recommendation;
 	}
 
-	public Integer getSalesVolum() {
-		return salesVolum;
-	}
-
-	public void setSalesVolum(Integer salesVolum) {
+	public void	setSalesVolum(Integer salesVolum){
 		this.salesVolum = salesVolum;
 	}
 
-	public String getcharacters() {
-		return characters;
-	}
-
-	public void setcharacters(String characters) {
+	public void	setCharacters(String characters){
 		this.characters = characters;
 	}
 
-	public String getProductFunction() {
-		return productFunction;
-	}
-
-	public void setProductFunction(String productFunction) {
+	public void	setProductFunction(String productFunction){
 		this.productFunction = productFunction;
 	}
 
-	public String getProductShape() {
-		return productShape;
-	}
-
-	public void setProductShape(String productShape) {
+	public void	setProductShape(String productShape){
 		this.productShape = productShape;
 	}
 
-	public Byte getProductIsDelete() {
-		return productIsDelete;
-	}
-
-	public void setProductIsDelete(Byte productIsDelete) {
+	public void	setProductIsDelete(Byte productIsDelete){
 		this.productIsDelete = productIsDelete;
 	}
 
@@ -159,6 +187,9 @@ public class ProductEntity{
 		return "ProductEntity{" +
 				"productId=" + productId +
 				", productName='" + productName + '\'' +
+				", pic1='" + pic1 + '\'' +
+				", pic2='" + pic2 + '\'' +
+				", pic3='" + pic3 + '\'' +
 				", price=" + price +
 				", number=" + number +
 				", introduction='" + introduction + '\'' +
